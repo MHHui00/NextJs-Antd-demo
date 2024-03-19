0a86ff491b5325e930a5bfd3b85e7a0fc1df516e
JustinHui <justinhui@genexis.com.hk>
2024-03-19 14:49:55 +0800

    main table template

--- app/page.js
+++ app/page.js
@@ -1,12 +1,115 @@
+"use client";
 import React from 'react';
-import { Button, Flex } from 'antd';
+import { Table } from 'antd';
 
-const Home = () => (
-  <Flex justify='space-evenly' align='center' vertical >
-      <Button type="primary">Button</Button>
-      <Button type="primary">Button2</Button>
-      <Button type="primary">Button3</Button>
-  </Flex>
-);
+ function Home() {
+  const columns = [
+    {
+      title: 'Full Name',
+      width: 100,
+      dataIndex: 'name',
+      key: 'name',
+      fixed: 'left',
+    },
+    {
+      title: 'Age',
+      width: 100,
+      dataIndex: 'age',
+      key: 'age',
+      fixed: 'left',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Column 1',
+      dataIndex: 'address',
+      key: '1',
+      width: 150,
+      
+    },
+    {
+      title: 'Column 2',
+      dataIndex: 'address',
+      key: '2',
+      width: 150,
+    },
+    {
+      title: 'Column 3',
+      dataIndex: 'address',
+      key: '3',
+      width: 150,
+    },
+    {
+      title: 'Column 4',
+      dataIndex: 'address',
+      key: '4',
+      width: 150,
+    },
+    {
+      title: 'Column 5',
+      dataIndex: 'address',
+      key: '5',
+      width: 150,
+    },
+    {
+      title: 'Column 6',
+      dataIndex: 'address',
+      key: '6',
+      width: 150,
+    },
+    {
+      title: 'Column 7',
+      dataIndex: 'address',
+      key: '7',
+      width: 150,
+    },
+    {
+      title: 'Column 8',
+      dataIndex: 'address',
+      key: '8',
+      width: 150,
+    },
+    {
+      title: 'Column 9',
+      dataIndex: 'address',
+      key: '9',
+      width: 150,
+    },
+    {
+      title: 'Column 10',
+      dataIndex: 'address',
+      key: '10',
+      width: 150,
+    },
+    {
+      title: 'Action',
+      key: 'operation',
+      fixed: 'right',
+      width: 100,
+      render: () => <a>Action</a>,
+    },
+  ];
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: `Edward ${i}`,
+      age: i,
+      address: `London Park no.${i} `,
+    });
+  }
+  return (
+    <>
+      <Table
+        columns={columns}
+        dataSource={data}
+        scroll={{
+          x: 1500,
+          y: 600,
+        }}
+      />
+    </>
+  );
+}
 
 export default Home;  
